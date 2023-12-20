679fa99b4c79bf69136512a8b1bf9fdb4408ff95
Vladyslav Vladinov <111443297+SalOne22@users.noreply.github.com>
2023-12-20 14:35:48 +0100

    format code

--- app/components/header/components/MobileMenu.tsx
+++ app/components/header/components/MobileMenu.tsx
@@ -5,7 +5,7 @@ import User from '@assets/icons/user.svg';
 export default function MobileMenu({ open }: { open: boolean }) {
   return (
     <div className="absolute bottom-0 translate-y-full py-[1.56rem] left-1/2 -translate-x-1/2 bg-alt-background w-[15.1875rem] text-center max-h-[calc(100dvh-3.875rem)] overflow-auto group-[[data-state='closed']]:opacity-0 transition-opacity duration-500">
-      <div className=" space-y-[1.125rem] flex flex-col items-center">
+      <div className="space-y-[1.125rem] flex flex-col items-center">
         <nav>
           <ul className="space-y-[1.125rem]">
             <li>
